cedec1fb696bc3b6ac847a41ad7d68e2352359ca
HouLingSWJ <30954632+HouLingSWJ@users.noreply.github.com>
2017-09-04 18:29:19 +0800

    增加一个粒子特效工具
    统一延时工具

---
+++ ParticalTool/DelayTool/ParticalDelayToolEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ParticleDelayTool))]
+public class ParticalDelayToolEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ParticleDelayTool myScript = (ParticleDelayTool)target;
+        if (GUILayout.Button("Save DelayTime"))
+        {
+            myScript.Add();
+        }
+    }
+}
+
---
+++ ParticalTool/DelayTool/ParticleDelayTool.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ParticleDelayTool : MonoBehaviour
+{
+    public int lastTime = 0;
+    public int time = 0;
+    public List<ParticleSystem> particleSystem = new List<ParticleSystem>(); 
+
+
+    [ExecuteInEditMode]
+    public void Add()
+    {
+        ChangeAllDelayTime(1);
+
+    }
+
+
+
+    //修改所有 延迟时间
+    public void ChangeAllDelayTime(int add)
+    {
+        ParticleDelayTool[] allDelayTool = GetComponents<ParticleDelayTool>();
+
+        for (int i = 0; i < allDelayTool.Length; i++)
+        {
+            allDelayTool[i].SaveSelfAndChild(add);
+        }
+    }
+
+    //保存 当前延迟时间
+    public void SaveSelfAndChild(int add)
+    {
+        for (int i = 0; i < particleSystem.Count; i++)
+        {
+            ParticleSystem[] l_particleSystemChild = particleSystem[i].transform.GetComponentsInChildren<ParticleSystem>();
+
+            for (int k = 0; k < l_particleSystemChild.Length; k++)
+            {
+                l_particleSystemChild[k].startDelay += ((time - lastTime) * add);
+            }
+            particleSystem[i].startDelay = time;
+
+            lastTime = time;
+
+
+        }
+    }
+
+}
+
