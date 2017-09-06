813fd4fb9d9f4ecee727120c3466c1912079a546
HouLingSWJ <30954632+HouLingSWJ@users.noreply.github.com>
2017-09-06 17:02:26 +0800

    增加Object Manager 中的 美术资源细分

--- ObjectManager/Editor/ObjectManagerWindow.cs
+++ ObjectManager/Editor/ObjectManagerWindow.cs
@@ -9,12 +9,12 @@ public class ObjectManagerWindow : EditorWindow {
  
     #region GUI
     static private Vector2 scrollView; //下拉区域
-    static readonly private string[] toolbarStrings = new string[] {"AssetBundle","Asset","GameObject","All" };
+    static readonly private string[] toolbarStrings = new string[] {"AssetBundle","Art Asset","GameObject","Others" };
     static private int toolbarSelect; //当前选择的tool 
     const int c_AssetBunld = 0;
-    const int c_Asset = 1;
+    const int c_ArtAsset = 1;
     const int c_GameObject = 2;
-    const int c_All = 3;
+    const int c_Others = 3;
 
     #endregion
 
@@ -24,15 +24,47 @@ public class ObjectManagerWindow : EditorWindow {
         GetWindow(typeof(ObjectManagerWindow));
     }
 
-    
     private void OnGUI()
     {
+        FindObjectByID();
         Bit_UpdateAllObjectInfo();
-
         ShowObjectInfo();
     }
 
     #region UI内方法
+
+
+    #region 根据ID 查找Obj
+    static private List<Object> allObject; //工程中所有Object
+    int aimObjetID; //希望寻找的Object ID
+    Object findAim;//希望寻找的Object
+    //展示所有Obj 信息
+    void FindObjectByID()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("请输入你要寻找的Object的Instance ID：", GUILayout.Width(300));
+        aimObjetID = int.Parse(GUILayout.TextField(aimObjetID.ToString()));
+
+        if (GUILayout.Button("查找"))
+        {
+            findAim = ObjectManager.GetObjectFromID(aimObjetID);
+        }
+        GUILayout.EndHorizontal();
+
+        if (findAim != null)
+        {
+            EditorGUILayout.ObjectField(findAim, typeof(Object), true);
+            GUILayout.Label(findAim.hideFlags.ToString());
+        }
+        else
+        {
+            GUILayout.Label("项目中没有Object:  " + aimObjetID);
+        }
+
+        GUILayout.Space(30);
+
+    }
+    #endregion
 
     //更新按钮  更新所要Object 信息
     void Bit_UpdateAllObjectInfo()
@@ -45,7 +77,9 @@ public class ObjectManagerWindow : EditorWindow {
                 case c_AssetBunld:
                     allAssetBundle = ObjectManager.GetSomeObject<AssetBundle>();
                     break;
-                case c_Asset: break;
+                case c_ArtAsset:
+                    UpdateArtAsset();
+                    break;
                 case c_GameObject:
                     allGameObject = ObjectManager.GetSomeObject<GameObject>();
                     InHierarchyGo.Clear();
@@ -62,7 +96,7 @@ public class ObjectManagerWindow : EditorWindow {
                         }
                     }
                     break;
-                case c_All:
+                case c_Others:
                     Object[] l_allObject = ObjectManager.GetAllObject();
                     if (l_allObject != null)
                     {
@@ -81,38 +115,12 @@ public class ObjectManagerWindow : EditorWindow {
         switch (toolbarSelect)
         {
             case c_AssetBunld: ShowAssetBundle(); break;
-            case c_Asset:break;
+            case c_ArtAsset: ShowAllArtAsset(); break;
             case c_GameObject: ShowAllGameObject(); break;
-            case c_All: FindObjectByID(); break;
+            case c_Others: ; break;
         }
         GUILayout.EndScrollView();
     }
-
-    #region 根据ID 查找Obj
-    static private List<Object> allObject; //工程中所有Object
-    int aimObjetID; //希望寻找的Object ID
-    Object findAim;//希望寻找的Object
-    //展示所有Obj 信息
-    void FindObjectByID()
-    {
-        aimObjetID = int.Parse(GUILayout.TextField(aimObjetID.ToString()));
-        
-        if (GUILayout.Button("查找"))
-        {
-            findAim = ObjectManager.GetObjectFromID(aimObjetID);
-        }
-        if (findAim != null)
-        {
-            EditorGUILayout.ObjectField(findAim, typeof(Object), true);
-            GUILayout.Label(findAim.hideFlags.ToString());
-        }
-        else
-        {
-            GUILayout.Label("项目中没有Object:  " + aimObjetID);
-        }
-
-    }
-    #endregion
 
     #region 展示所有assetBundle
     static private List<Object> allAssetBundle;//所有assetbundle
@@ -147,6 +155,60 @@ public class ObjectManagerWindow : EditorWindow {
         }
 
         
+    }
+
+    #endregion
+
+    #region 展示所有Asset 包括 Mesh 、 Material  、Texture
+
+    static readonly private string[] assetToolbarStrings = new string[] { "Material", "Texture", "Mesh" };
+    static private int assetToolbarSelect = 0; //当前选择的tool
+
+    const int c_Material = 0;
+    const int c_Texture = 1;
+    const int c_Mesh = 2;
+
+    //展示所有美术资源
+    private void ShowAllArtAsset()
+    {
+        assetToolbarSelect = GUILayout.Toolbar(assetToolbarSelect, assetToolbarStrings);
+        switch (assetToolbarSelect)
+        {
+            case c_Material: ShowAllMaterial(); break;
+            case c_Texture: ShowAllTexture(); break;
+            case c_Mesh: ShowAllMesh(); break;
+        }
+    }
+
+    //更新美术资源列表
+    private void UpdateArtAsset()
+    {
+        switch (assetToolbarSelect)
+        {
+            case c_Material:
+                break;
+            case c_Texture:
+                break;
+            case c_Mesh:
+                break;
+        }
+    }
+
+    //展示所有材质
+    private void ShowAllMaterial()
+    {
+
+    }
+
+    //展示所有Texture
+    private void ShowAllTexture()
+    {
+    }
+
+    //展示所有Mesh
+    private void ShowAllMesh()
+    {
+
     }
 
     #endregion
