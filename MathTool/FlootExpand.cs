d0aedc8986987fadf2fc19dcb0ef2375c7da70a5
HouLingSWJ <30954632+HouLingSWJ@users.noreply.github.com>
2018-01-02 18:04:34 +0800

    修改四舍五入方法，兼容负数

---
+++ MathTool/FlootExpand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FlootExpand {
+
+    //四舍五入
+    public static int Round45(this float value)
+    {
+        //Debug.Log("==Round45== value ==" + value);
+        if (value > 0)
+        {
+            value += 0.49f;
+        }
+        else
+        {
+            value -= 0.49f;
+        }
+        //Debug.Log("==Round45==" + value);
+        return (int)(value);
+    }
+
+
+}
