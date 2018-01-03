a3d46dad4bb54197f658dd181006fa0dd79d89c2
HouLingSWJ <30954632+HouLingSWJ@users.noreply.github.com>
2018-01-03 11:53:21 +0800

    提交运动模糊后期特效

---
+++ PostProcess/MotionBlur/MotionBlur.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionBlur : SupportedBese {
+
+    public Shader motionBlurShader;
+    [Tooltip("混合系数")][Range(0,0.9f)]
+    public float blend;
+    private Material motionBlurMaterial;
+
+    public RenderTexture lastTex = null;
+
+    private void Awake()
+    {
+        //Application.targetFrameRate = 25; //限制帧率，但是要注意关掉垂直同步
+    }
+
+    private Material material
+    {
+        get {
+            motionBlurMaterial = ChackShaderAndCreatMaterial(motionBlurShader, motionBlurMaterial);
+            return motionBlurMaterial;
+        }
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material != null)
+        {
+            if (lastTex == null || lastTex.width != source.width || lastTex.height != source.height)
+            {
+                DestroyImmediate(lastTex);
+                lastTex = new RenderTexture( source.width, source.height, 0);
+                lastTex.filterMode = FilterMode.Bilinear;
+                Graphics.Blit(source, lastTex);        
+                
+            }
+            //lastTex.MarkRestoreExpected();//移动端代价昂贵，其实就是一个释放，可以自己管理
+            material.SetFloat("_Blend", blend);
+
+            Graphics.Blit(source, lastTex, material);
+            Graphics.Blit(lastTex, destination);
+            
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        DestroyImmediate(lastTex);
+    }
+}
---
+++ PostProcess/MotionBlur/MotionBlur.shader
@@ -0,0 +1,85 @@
+﻿Shader "Unlit/MotionBlur"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Blend("Blend",float) = 0.5
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" "queue" = "Transparent" }
+		LOD 100
+
+		ZTest Always cull off ZWrite off
+
+
+		CGINCLUDE
+		
+		#include "UnityCG.cginc"
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			float2 uv : TEXCOORD0;
+		};
+
+		struct v2f
+		{
+			float2 uv : TEXCOORD0;
+			float4 vertex : SV_POSITION;
+		};
+
+		sampler2D _MainTex;
+		float4 _MainTex_ST;
+		float _Blend;
+
+		v2f vert (appdata v)
+		{
+			v2f o;
+			o.vertex = UnityObjectToClipPos(v.vertex);
+			o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+			return o;
+		}
+			
+		fixed4 frag (v2f i) : SV_Target
+		{
+			fixed4 col = tex2D(_MainTex, i.uv);
+			col.a = _Blend;
+			return col;
+		}
+
+		fixed4 frag2 (v2f i) : SV_Target
+		{
+			fixed4 col = tex2D(_MainTex, i.uv);
+
+			return col;
+		}
+
+		ENDCG
+
+		Pass
+		{
+			blend SrcAlpha OneMinusSrcAlpha
+			ColorMask rgb
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			//利用透明度a进行混合
+			ENDCG
+		}
+
+		Pass
+		{
+			blend one zero
+			ColorMask a
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag2
+
+			//本通道 为了 保护原有的a 值，不被_Blend覆盖掉
+			ENDCG
+		}
+	}
+
+	FallBack Off
+}
